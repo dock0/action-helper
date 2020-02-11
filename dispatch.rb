bc9a8a662e9e877a6841793f41beceec9291803f
Les Aker <me@lesaker.org>
2020-02-11 15:52:23 +0000

    add dispatch.rb

---
+++ dispatch.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require 'uri'
+require 'net/http'
+require 'json'
+
+repo = ENV['GITHUB_REPOSITORY']
+
+uri = URI("https://api.github.com/repos/#{repo}/dispatches")
+req = Net::HTTP.post(
+  uri,
+  { 'event-type' => 'update-rebuild' }.to_json,
+  { 'Authorization' => "bearer #{ENV['GITHUB_TOKEN']}" }
+)
+fail("Failed query: #{req.code}") unless req.code_type == Net::HTTPOK
