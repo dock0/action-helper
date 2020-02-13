47805dd16d37a8c0a200db3e6386ad2e496418f6
Les Aker <me@lesaker.org>
2020-02-13 09:00:21 +0000

    update cops

--- get_latest.rb
+++ get_latest.rb
@@ -4,39 +4,45 @@ require 'uri'
 require 'net/http'
 require 'json'
 
-def get_latest(src)
-  match = src.match(/docker\.pkg\.github\.com\/(\w+)\/(\w+)\/(\w+):(\w+)/)
-  fail('failed to parse package') unless match
-  org, repo, image, _ = match.captures
-
+def build_query(org, repo, image)
   query = "query {
     repository(owner:\"#{org}\", name:\"#{repo}\"){
       packages(names:[\"#{image}\"], first: 1) {
         nodes {
-          latestVersion {
-            version
-          }
+          latestVersion { version }
         }
       }
     }
   }"
+  { 'query' => query }.to_json
+end
 
+def parse_source(src)
+  match = src.match(%r{docker\.pkg\.github\.com/(\w+)/(\w+)/(\w+):(\w+)})
+  raise('failed to parse package') unless match
+  match.captures
+end
+
+def make_request(body)
   uri = URI('https://api.github.com/graphql')
   req = Net::HTTP.post(
     uri,
-    { 'query' => query }.to_json,
-    {
-      'Accept' => 'application/vnd.github.packages-preview+json',
-      'Authorization' => "bearer #{ENV['GITHUB_TOKEN']}"
-    }
+    body,
+    'Accept' => 'application/vnd.github.packages-preview+json',
+    'Authorization' => "bearer #{ENV['GITHUB_TOKEN']}"
   )
-  fail("Failed GraphQL lookup: #{req.code}") unless req.code_type == Net::HTTPOK
+  return JSON.parse(req.body) if req.code_type == Net::HTTPOK
+  raise("Failed GraphQL lookup: #{req.code}")
+end
 
-  resp = JSON.parse(req.body)
+def get_latest(src)
+  org, repo, image, = parse_source(src)
+  query = build_query(org, repo, image)
+  resp = make_request(query)
   resp['data']['repository']['packages']['nodes'][0]['latestVersion']['version']
 end
 
-if caller.length == 0
-  src = ARGV.shift || fail('image path not provided')
+if caller.empty?
+  src = ARGV.shift || raise('image path not provided')
   puts get_latest(src)
 end
--- update.rb
+++ update.rb
@@ -7,17 +7,34 @@ require 'json'
 require_relative 'get_latest.rb'
 
 def commit(msg)
-  system('git', 'config', '--local', 'user.email', 'action@github.com') || fail('failed to set email')
-  system('git', 'config', '--local', 'user.name', 'Github Action') || fail('failed to set name')
-  system('git', 'commit', '-m', msg) || fail('failed to git commit')
-  system('git', 'push', 'origin', 'master') || fail('failed to git push')
-  puts "::set-output name=updated::yes"
+  ok = system('git', 'config', '--local', 'user.email', 'action@github.com')
+  raise('failed to set email') unless ok
+  ok = system('git', 'config', '--local', 'user.name', 'Github Action')
+  raise('failed to set name') unless ok
+  ok = system('git', 'commit', '-m', msg)
+  raise('failed to git commit') unless ok
+  ok = system('git', 'push', 'origin', 'master')
+  raise('failed to git push') unless ok
+  puts '::set-output name=updated::yes'
+end
+
+def update_dockerfile(org, repo, image, version) # rubocop:disable Metrics/MethodLength
+  old = File.read('Dockerfile').lines
+  breaker = false
+  File.open('Dockerfile', 'w') do |fh|
+    old.each do |line|
+      if !breaker && line =~ /^FROM docker\.pkg\.github\.com/
+        fh << "FROM docker.pkg.github.com/#{org}/#{repo}/#{image}:#{version}\n"
+        breaker = true
+      else
+        fh << line
+      end
+    end
+  end
 end
 
 def github_registry_bump(src)
-  match = src.match(/docker\.pkg\.github\.com\/([\w-]+)\/([\w-]+)\/([\w-]+):([\w-]+)/)
-  fail('failed to parse package') unless match
-  org, repo, image, current = match.captures
+  org, repo, image, current = parse_source(src)
   latest = get_latest(src)
 
   if latest == current
@@ -25,38 +42,26 @@ def github_registry_bump(src)
     return
   end
 
-  old = File.read('Dockerfile').lines
-  breaker = false
-  File.open('Dockerfile', 'w') do |fh|
-    old.each do |line|
-      if !breaker && line =~ /^FROM docker\.pkg\.github\.com/
-        fh << "FROM docker.pkg.github.com/#{org}/#{repo}/#{image}:#{latest}\n"
-        breaker = true
-      else
-        fh << line
-      end
-    end
-  end
-
-  system('git', 'add', 'Dockerfile') || fail('failed to git add')
+  update_dockerfile(org, repo, image, latest)
+  system('git', 'add', 'Dockerfile') || raise('failed to git add')
   commit "Bumped source to #{latest}"
 end
 
 def scratch_bump
-  datestamp = DateTime.now.strftime("%Y%m%d-%H%M%S")
+  datestamp = DateTime.now.strftime('%Y%m%d-%H%M%S')
   File.open('stamp', 'w') { |fh| fh << datestamp }
-  system('git', 'add', 'stamp') || fail('failed to git add')
+  system('git', 'add', 'stamp') || raise('failed to git add')
   commit "Bumping on #{datestamp}"
 end
 
 src_image = File.read('Dockerfile').lines.grep(/^FROM/).first.split[1]
 
 case src_image
-when "scratch"
+when 'scratch'
   scratch_bump
 when /^docker.pkg.github.com/
   github_registry_bump src_image
 else
-  fail("Unknown source image: #{src_image}") unless File.exist? 'stamp'
+  raise("Unknown source image: #{src_image}") unless File.exist? 'stamp'
   scratch_bump
 end
