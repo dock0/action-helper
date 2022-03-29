97baccad24950e0b7f7a316a96f3e7b8da02bdcf
Les Aker <me@lesaker.org>
2022-03-29 17:34:02 -0400

    switch to REST API

--- get_latest.rb
+++ get_latest.rb
@@ -4,19 +4,6 @@ require 'uri'
 require 'net/http'
 require 'json'
 
-def build_query(org, repo, image)
-  query = "query {
-    repository(owner:\"#{org}\", name:\"#{repo}\"){
-      packages(names:[\"#{image}\"], first: 1) {
-        nodes {
-          latestVersion { version }
-        }
-      }
-    }
-  }"
-  { 'query' => query }.to_json
-end
-
 DOCKER_SRC = %r{ghcr\.io/([\w-]+)/([\w-]+)/([\w-]+):([\w-]+)}
 
 def parse_source(src)
@@ -25,23 +12,24 @@ def parse_source(src)
   match.captures
 end
 
-def make_request(body)
-  uri = URI('https://api.github.com/graphql')
-  req = Net::HTTP.post(
+def make_request(org, repo, image)
+  uri = URI("https://api.github.com/orgs/#{org}/packages/container/#{repo}%2F#{image}/versions")
+  req = Net::HTTP.get_response(
     uri,
-    body,
-    'Accept' => 'application/vnd.github.packages-preview+json',
-    'Authorization' => "bearer #{ENV['GITHUB_TOKEN']}"
+    'Accept' => 'application/vnd.github.v3+json',
+    'Authorization' => "token #{ENV['GITHUB_TOKEN']}"
   )
   return JSON.parse(req.body) if req.code_type == Net::HTTPOK
-  raise("Failed GraphQL lookup: #{req.code}")
+  raise("Failed API lookup: #{req.code}")
 end
 
 def get_latest(src)
   org, repo, image, = parse_source(src)
-  query = build_query(org, repo, image)
-  resp = make_request(query)
-  resp['data']['repository']['packages']['nodes'][0]['latestVersion']['version']
+  resp = make_request(org, repo, image)
+  tags = resp.first.dig('metadata', 'container', 'tags')
+  # this checks if latest is in the tag list *and* removes it, leaving just the datestamp tag
+  raise("Latest image doesn't have latest tag") unless tags.delete 'latest'
+  return tags.first
 end
 
 if caller.empty?
