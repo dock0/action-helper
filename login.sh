ddd8b36128df06c6748b394881139a7c98e83b9b
Les Aker <me@lesaker.org>
2022-03-29 17:40:42 -0400

    fix registry name

--- login.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-docker login docker.pkg.github.com -u nobody -p "${GITHUB_TOKEN}"
-
--- publish.sh
+++ publish.sh
@@ -8,7 +8,7 @@ PATH="${curdir}:${PATH}"
 login.sh
 
 for tag in $(get_tags.sh) ; do
-    full_name="docker.pkg.github.com/${GITHUB_REPOSITORY}:${tag}"
+    full_name="ghcr.io/${GITHUB_REPOSITORY}:${tag}"
     echo "Tagging ${full_name}"
     docker tag new "${full_name}"
     docker push "${full_name}"
